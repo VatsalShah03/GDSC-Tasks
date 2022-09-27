3ff3451166bcf07b720a3606fe811735c5c26a21
vsshah2003 <vsshah2003@gmail.com>
2022-09-27 06:24:18 +0530

    Final Commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,15 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:introduction_screen/introduction_screen.dart';
+import 'package:gdsc_tasks/signin_page.dart';
 
 class IntroScreen extends StatelessWidget {
-  const IntroScreen({Key? key}) : super(key: key);
+  final String email;
+  const IntroScreen({Key? key, required this.email}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IntroductionScreen(
-        done: Text("Done"),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("Assets/bg-1.jpg"), fit: BoxFit.fill)),
+      child: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const SizedBox(width: 20,),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.white),
+                  onPressed: () {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => const SignInPage()));
+                  },
+                  child: const Text("Sign Out", style: TextStyle(color: Colors.deepPurpleAccent, fontWeight: FontWeight.bold),),
+                )
+              ],
+            ),
+            Expanded(child: Center(
+              child: Container(
+                padding: const EdgeInsets.only(left: 20),
+                alignment: Alignment.centerLeft,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Welcome!", style: TextStyle(color: Colors.white, fontSize: 60, fontWeight: FontWeight.w900),),
+                    Text(email, style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.w500),),
+
+                  ],
+                ),
+              ),
+            ))
+          ],
+        ),
       ),
-    );
+    ));
   }
 }
--- lib/signin_page.dart
+++ lib/signin_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gdsc_tasks/home_page.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({Key? key}) : super(key: key);
@@ -14,7 +15,6 @@ class _SignInPageState extends State<SignInPage> {
   bool isVisible = false;
   @override
   Widget build(BuildContext context) {
-
     final Shader linearGradient = const LinearGradient(
       colors: <Color>[
         Color.fromRGBO(195, 90, 226, 1),
@@ -24,8 +24,10 @@ class _SignInPageState extends State<SignInPage> {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
 
-    final Gradient button = LinearGradient(colors: [Color.fromRGBO(195, 90, 226, 1),
-        Color.fromRGBO(7, 180, 149, 1)]);
+    final Gradient button = LinearGradient(colors: [
+      Color.fromRGBO(195, 90, 226, 1),
+      Color.fromRGBO(7, 180, 149, 1)
+    ]);
 
     return Scaffold(
       backgroundColor: Colors.grey[300],
@@ -58,13 +60,16 @@ class _SignInPageState extends State<SignInPage> {
                 mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-              Center(
-               child: CircleAvatar(backgroundImage: AssetImage("Assets/logo.jpg"),radius: 60,),
-              ),
+                  Center(
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage("Assets/logo.jpg"),
+                      radius: 60,
+                    ),
+                  ),
                   ClipRRect(
                       borderRadius: BorderRadius.circular(20),
-                      child:
-                          Image.asset("Assets/gdsc_djsce.jpg", width: width * 0.8)),
+                      child: Image.asset("Assets/gdsc_djsce.jpg",
+                          width: width * 0.8)),
                   Container(
                     width: width * 0.8,
                     alignment: AlignmentDirectional.bottomCenter,
@@ -85,7 +90,9 @@ class _SignInPageState extends State<SignInPage> {
                               letterSpacing: 1.5,
                               wordSpacing: 3),
                         ),
-                        SizedBox(height: 30,),
+                        SizedBox(
+                          height: 30,
+                        ),
                         Form(
                           key: _formKey,
                           child: Column(
@@ -96,28 +103,29 @@ class _SignInPageState extends State<SignInPage> {
                                   if (text == null || text.isEmpty) {
                                     return 'Email cannot be empty';
                                   }
-                                  if(text.contains("@")){
+                                  if (text.contains("@")) {
                                     return null;
-                                  }
-                                  else{
+                                  } else {
                                     return "Email should contain @";
                                   }
                                 },
                                 keyboardType: TextInputType.emailAddress,
                                 decoration: InputDecoration(
-                                  enabledBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(12),
-                                      borderSide: BorderSide(color: Colors.purple)
-                                  ),
-                                  focusedBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(12),
-                                      borderSide: BorderSide(color: Colors.purple)
-                                  ),
-                                  labelText: 'Email',
-                                  labelStyle: TextStyle(color: Colors.deepPurple)
-                                ),
+                                    enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(12),
+                                        borderSide:
+                                            BorderSide(color: Colors.purple)),
+                                    focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(12),
+                                        borderSide:
+                                            BorderSide(color: Colors.purple)),
+                                    labelText: 'Email',
+                                    labelStyle:
+                                        TextStyle(color: Colors.deepPurple)),
                               ),
-                              SizedBox(height: 20,),
+                              SizedBox(
+                                height: 20,
+                              ),
                               TextFormField(
                                 obscureText: isVisible,
                                 controller: _passwordController,
@@ -129,44 +137,67 @@ class _SignInPageState extends State<SignInPage> {
                                 },
                                 keyboardType: TextInputType.text,
                                 decoration: InputDecoration(
-                                  suffixIcon: InkWell(
-                                    onTap: (){
-                                      setState(() {
-                                        print("Function Called");
-                                        isVisible = !isVisible;
-                                      });
-                                    },
-                                    child: isVisible ? Icon(Icons.visibility, color: Colors.black,) : Icon(Icons.visibility_off, color: Colors.black,),
-                                  ),
+                                    suffixIcon: InkWell(
+                                      onTap: () {
+                                        setState(() {
+                                          print("Function Called");
+                                          isVisible = !isVisible;
+                                        });
+                                      },
+                                      child: isVisible
+                                          ? Icon(
+                                              Icons.visibility,
+                                              color: Colors.black,
+                                            )
+                                          : Icon(
+                                              Icons.visibility_off,
+                                              color: Colors.black,
+                                            ),
+                                    ),
                                     enabledBorder: OutlineInputBorder(
                                         borderRadius: BorderRadius.circular(12),
-                                        borderSide: BorderSide(color: Colors.purple)
-                                    ),
+                                        borderSide:
+                                            BorderSide(color: Colors.purple)),
                                     focusedBorder: OutlineInputBorder(
                                         borderRadius: BorderRadius.circular(12),
-                                        borderSide: BorderSide(color: Colors.purple)
-                                    ),
+                                        borderSide:
+                                            BorderSide(color: Colors.purple)),
                                     labelText: 'Password',
-                                    labelStyle: TextStyle(color: Colors.deepPurple)
-                                ),
+                                    labelStyle:
+                                        TextStyle(color: Colors.deepPurple)),
                               ),
-                              SizedBox(height: 20,),
+                              SizedBox(
+                                height: 20,
+                              ),
                               InkWell(
-                                onTap: (){
-                                  _formKey.currentState?.validate();
+                                onTap: () {
+                                  if (_formKey.currentState!.validate()) {
+                                    Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                IntroScreen(email: _emailController.text.trim(),)));
+                                  }
+                                  ;
                                 },
                                 child: Container(
                                   padding: EdgeInsets.all(15),
                                   width: double.infinity,
                                   decoration: BoxDecoration(
-                                    gradient: button,
-                                    borderRadius: BorderRadius.circular(10)
-                                  ),
+                                      gradient: button,
+                                      borderRadius: BorderRadius.circular(10)),
                                   child: Row(
                                     mainAxisAlignment: MainAxisAlignment.center,
                                     children: const [
-                                      Text("Submit  ", style: TextStyle(color: Colors.white, fontSize: 20),),
-                                      Icon(Icons.chevron_right, color: Colors.white,),
+                                      Text(
+                                        "Submit  ",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 20),
+                                      ),
+                                      Icon(
+                                        Icons.chevron_right,
+                                        color: Colors.white,
+                                      ),
                                     ],
                                   ),
                                 ),
